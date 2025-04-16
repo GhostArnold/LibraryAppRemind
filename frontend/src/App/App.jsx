624ee7ff53609bed50f094fe83974079e1f2da96
Artem <prudnikov-artyom@list.ru>
2025-04-16 19:03:59 +0300

    Start template

---
+++ frontend/src/App/App.jsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="app-header">
+        <h1>Book Library App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
